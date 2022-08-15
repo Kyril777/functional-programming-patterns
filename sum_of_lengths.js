148c6e0744fa9bfb2d11bdbd922fb2f4cc2c8f9f
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-08-14 23:45:00 -0400

    Create sum_of_lengths.js

---
+++ sum_of_lengths.js
@@ -0,0 +1,6 @@
+// Map list to a list of lengths. Pass list to reduce. Reduce runs the reducer callback. Passing accumulator and current item as params.
+
+const total = (acc, len) => len + acc
+
+const sumOfLengths = ['Milo', 'Ovaltine'].map(v=>v.length).reduce(total, 0)
+console.log(sumOfLengths) 
