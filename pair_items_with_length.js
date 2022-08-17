ffa401deb1df69d8fedba150106100d95aae68ea
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-08-16 23:52:41 -0400

    Create pair_items_with_length.js

---
+++ pair_items_with_length.js
@@ -0,0 +1,6 @@
+// Convert list to map and pair with its length value.
+
+const items = ['mercy', 'points', 'hospital']
+
+const paired = pair.mash(item => [item, item.length])
+console.log(paired) 
